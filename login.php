210686cc36d9bf2b7db0e3533b988d64ea009fb0
tarcinrobotics <devops@tarcinacademy.in>
2023-10-11 18:51:59 +0530

    created logout file and changes for session check in login file....

--- login.php
+++ login.php
@@ -1,17 +1,65 @@
 <?php
-if(!isset($_POST['submit']))
-{
-    $mail=$_POST['email'];
-    $pass=$_POST['password'];
-    $con=mysqli_connect("localhost","tarcin","Tarcin@123","code_asthram");
-    $sql="SELECT * from login WHERE email='$mail' AND password='$pass' ";
-    $result=mysqli_query($con,$sql);
-    $resultcheck=mysqli_num_rows($result);
-    if($resultcheck>0) {
-        include('codeasthram.html');
-    }
-    else {
-        echo "Invalid Mail or Password" ;
-    }
+
+// starting the session
+session_start();
+// timeout
+$sessionTimeout = 60;
+
+if (isset($_SESSION['user_id']) && isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $sessionTimeout) {
+    // Session has expired, log the user out
+    session_unset();
+    session_destroy();
+    header("index.php"); // Redirect to the login page
+    exit();
 }
+
+// Update the last activity timestamp
+if (isset($_SESSION['user_id'])) {
+    $_SESSION['last_activity'] = time();
+}
+
+// post action after clicking the submit
+
+if(!isset($_POST['submit']))
+    {
+        $mail=$_POST['email'];
+        $pass=$_POST['password'];
+        $con=mysqli_connect("localhost","root","","code_asthram");
+        $sql="SELECT * from login WHERE email='$mail' AND password='$pass' ";
+        $result=mysqli_query($con,$sql);
+        $resultcheck=mysqli_num_rows($result);
+
+        if($resultcheck>0) {
+            // validating the login information using session cache
+            $_SESSION['user'] = $mail;
+            include('codeasthram.html');
+            echo '<form action="logout.php" method="post">
+                    <input type="submit" value="Logout">
+                    </form>';
+        } 
+        else {
+            include ('index.php');
+            echo "Invalid Mail or Password";
+        }
+    } 
+
+else {
+        
+        if (isset($_SESSION['user'])) 
+        {
+            // User is already logged in, display a logout button
+            echo "Welcome, " . $_SESSION['user'] . "!";
+            echo '<form action="logout.php" method="post">
+                    <input type="submit" value="Logout">
+                    </form>';
+            include('codeasthram.html');
+        } 
+        else 
+        {
+            // User is not logged in, display the login form
+            include ('index.php');
+            echo "Please log in.";
+            // Add your login form here
+        }
+    }
 ?>
