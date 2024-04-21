d921d5caea64429edd1fd7b40f1824241998d138
vignesh-pandian-p <vigneshpandian0710@gmail.com>
2024-04-21 14:05:23 -0700

    config file updated to cloud mongo db

--- src/config.js
+++ src/config.js
@@ -1,32 +1,4 @@
 const mongoose = require("mongoose");
-const connect = mongoose.connect("mongodb://localhost:27017/code_asthram");
-
-//To check database connected or not
-connect.then(() =>{
-    console.log("Database connected Successfully");
-})
-.catch(() =>{
-    console.log("Database cannot be connected");
-});
-
-//create a schema
-const LoginSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-});
-
-//collection part
-const collection = new mongoose.model("users", LoginSchema);
-
-module.exports = collection;
-/*
-const mongoose = require("mongoose");
 
 // MongoDB Atlas connection string
 const mongoURI = "mongodb+srv://tarcinrobotics301:tarcinrobotics301@cluster0.kpaipm9.mongodb.net/?retryWrites=true&w=majority";
@@ -35,16 +7,27 @@ const mongoURI = "mongodb+srv://tarcinrobotics301:tarcinrobotics301@cluster0.kpa
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-});
-
-// Get the default connection
-const db = mongoose.connection;
-
-// Event listeners for database connection
-db.on("error", console.error.bind(console, "Connection error:"));
-db.once("open", () => {
+})
+.then(() => {
   console.log("Database connected successfully");
+})
+.catch((err) => {
+  console.error("Connection error:", err);
 });
 
-// Export the connection
-module.exports = db;*/
+// Create a schema
+const LoginSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+// Define the collection
+const collection = mongoose.model("User", LoginSchema);
+
+module.exports = collection;
